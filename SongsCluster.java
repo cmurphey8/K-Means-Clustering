757e797440b6f4fc60dce386afe0d14662d01c8a
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-03-07 15:57:16 -0500

    Add files via upload

---
+++ SongsCluster.java
@@ -0,0 +1,128 @@
+//*******************************************************************
+//
+//   File: TaskMan.java
+//
+//   Author: CS112      Email:  
+//
+//   Class: NBody 
+// 
+//   Time spent on this problem: 
+//   --------------------
+//   
+//      This program 
+//
+//*******************************************************************
+
+import java.awt.Color;
+import java.io.FileNotFoundException;
+
+public class SongsCluster {
+
+    // init Panel
+    static final int WIDTH = 600;
+    static final int HEIGHT = 400;
+
+    // declare data arrays
+    public static String[] name;    // song name
+    public static int[] dance;      // danceability (y coordinate)
+    public static int[] energy;     // energy       (x coordinate)
+    public static int[] cluster;    // cluster assignments
+
+    // declare centroid arrays
+    public static int[] cx;         // centroid x-coordinates
+    public static int[] cy;         // centroid y-coordinates
+    public static Color[] color;    // centroid pen color
+
+    // declare number of centroids to form clusters around
+    public static int numClusters = 6;
+    
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // init arrays
+        SongsHelper.loadSongs();
+        SongsHelper.initCentroids();
+
+        // init algorithm stop condition
+        double sumDist;
+
+        do {
+            // YOU DO: complete helper method findMin to assign each song to its nearest centroid
+            assignSongs();
+
+            // YOU DO: complete method to update centroid coordinates
+            sumDist = shiftCentroids();
+
+            // plot centroid coordinates
+            SongsHelper.update();
+
+        } while (sumDist > 0.05);
+        System.out.println("~fin");
+    }
+
+    // YOU DO: complete method to update centroids
+    public static double shiftCentroids() {
+        // declare arrays to accumulate centroid data
+        int[] coordSumsX = new int[numClusters];    // sum of x-coordinate values for all songs in each cluster 
+        int[] coordSumsY = new int[numClusters];    // sum of y-coordinate values for all songs in each cluster
+        int[] coordCounts = new int[numClusters];   // number of songs in each cluster
+        
+        // YOU DO: store sum of x, y song coordinate values for each cluster in coordSumsX, coordSumsY
+        // YOU DO: store total number of songs for each cluster in coordCounts 
+        for (int i = 0; i < cluster.length; i++) {
+            coordCounts[cluster[i]]++;
+            coordSumsX[cluster[i]] += energy[i];
+            coordSumsY[cluster[i]] += dance[i];
+        }
+
+        // init counter to sum deltas from all centroid shifts
+        double sum = 0;
+
+        // update centoid arrays with average x, y coordinates for their clusters
+        for (int i = 0; i < numClusters; i++) {
+            // store old values
+            int cx0 = cx[i];
+            int cy0 = cy[i];
+
+            // update new values
+            if (coordCounts[i] > 0) {
+                cx[i] = (int) ((double) coordSumsX[i] / coordCounts[i]);
+                cy[i] = (int) ((double) coordSumsY[i] / coordCounts[i]);    
+            }
+            
+            // sum all deltas from centroid shifts
+            sum += SongsHelper.distance(cx0, cy0, cx[i], cy[i]); 
+        }
+        return sum;    
+    }
+
+    // assign each song to its nearest centroid
+    public static void assignSongs() {
+
+        // loop over all songs
+        for (int i = 0; i < cluster.length; i++) {
+            // declare distance data array
+            double[] dist = new double[numClusters];
+
+            // load distance data into array 
+            for (int j = 0; j < numClusters; j++) {
+                dist[j] = SongsHelper.distance(energy[i], dance[i], cx[j], cy[j]);
+            }
+            cluster[i] = findMin(dist);
+        }
+    }
+    
+    // YOU DO: complete method to return the index of centroid with minimum distance
+    public static int findMin(double[] dist) {
+        // init vars to track index and value of minimum distance in dist array
+        int minIndex = 0;
+        double minDist = dist[0];
+        for (int i = 1; i < dist.length; i++) {
+            // YOU DO: find index and value of the minimum distance in the dist array
+            if (dist[i] < minDist) {
+                minIndex = i;
+                minDist = dist[i];
+            }
+        }
+        return minIndex;
+    }
+}   
---
+++ SongsHelper.java
@@ -0,0 +1,149 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SongsHelper extends SongsCluster {
+    
+    // init drawing panel
+    static DrawingPanel panel = new DrawingPanel(WIDTH, HEIGHT);
+    static Graphics2D g = panel.getGraphics();
+
+    // enable double buffering
+    static BufferedImage offscreen = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    static Graphics2D osg = offscreen.createGraphics();
+    
+
+    // update plots
+    public static void update() {
+        osg.setColor(Color.BLACK);
+        osg.fillRect(0,0,WIDTH, HEIGHT);
+        graphCentroids();
+        graphSongs();
+    }
+            
+    // graph centroids
+    public static void graphCentroids() {
+        // size of circle to center over x, y coordinate pairs 
+        final int size = 4;
+
+        // plot all centroids, color-coded
+        for (int i = 0; i < numClusters; i++) {
+
+            // set osg pen color assigned to centroid
+            osg.setColor(color[i]);
+
+            // find coordinates for circle centered over plot-point
+            int x = (int) (cx[i] - size / 2.0);
+            int y = (int) (cy[i] + size / 2.0) ;
+            
+            // plot centroid to osg
+            osg.drawOval(x, y, size, size);    
+        }
+    }
+
+    // graph energy vs dance for each song
+    public static void graphSongs() {
+        // size of circle to center over each plot-point 
+        final int size = 2;
+
+        // plot all songs, color-coded by nearest centroid
+        for (int i = 0; i < name.length; i++) {
+
+            // set osg pen color by cluster assignment
+            osg.setColor(color[cluster[i]]);
+
+            // find x, y arguments for circle centered over plot-point
+            int x = (int) (energy[i] - size / 2.0);
+            int y = (int) (dance[i] + size / 2.0) ;
+            
+            // plot song to osg
+            osg.fillOval(x, y, size, size);      
+        }
+        // copy off screen image onto DrawingPanel
+        g.drawImage(offscreen, 0, 0, null);  
+
+        // wait 500 ms (1/2 second)
+        panel.sleep(500);
+    }
+
+    // read in song attributes
+    public static void loadSongs() throws FileNotFoundException {
+        
+        // init scanner
+        Scanner input = new Scanner(new File("songs.txt"));
+
+        // move to next word with standard (whitespace) delimiter
+        input.next();
+
+        // record number of songs
+        int N = Integer.parseInt(input.next());
+
+        // skip 2 fields to move on to first line of data
+        input.nextLine();  
+        input.nextLine();  
+
+        // set delimiter from whitespace to commas
+        input.useDelimiter(",");
+
+        // define array size
+        name = new String[N];
+        dance = new int[N];
+        energy = new int[N];
+        cluster = new int[N];
+
+        // read N tasks into arrays name, dance, energy, and length
+        for (int i = 0; i < N; i++) {
+            // move from id to name
+            input.next();
+
+            // load song name
+            name[i] = input.next();
+
+            // move from artist_id to dance
+            input.next();
+
+            // import dance (y) and energy (x), scaled to the Panel size
+            dance[i] = (int) (HEIGHT - input.nextDouble() * HEIGHT);
+            energy[i] = (int) (input.nextDouble() * WIDTH) ;
+
+            // move to next line
+            input.nextLine();
+        }
+
+        // close .txt file
+        input.close();
+    }
+
+    // set random initial centroid coordinates
+    public static void initCentroids() {
+
+        // initialize all centroid arrays to size numClusters
+        color = new Color[numClusters];
+        cx = new int[numClusters];
+        cy = new int[numClusters];
+        
+        // random numbers in range helper constants
+        final double remPercent = .25;
+        final int maxColor = 255;
+
+        // set random centroid locations and color assignments, excluding remPercent% of extreme values
+        for (int i = 0; i < numClusters; i++) {    
+            cx[i] = rand(WIDTH, remPercent);
+            cy[i] = rand(HEIGHT, remPercent);
+            color[i] = new Color(rand(maxColor, remPercent), rand(maxColor, remPercent), rand(maxColor, remPercent));
+        }
+    }
+    
+    // rng in range helper method
+    public static int rand(int max, double remPercent){
+        return (int) (Math.random() * ((1 - remPercent) * max) + remPercent / 2 * max);
+    }
+
+    // standard L2 norm - root of sum of squares distance formula
+    public static double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+    } 
+}
